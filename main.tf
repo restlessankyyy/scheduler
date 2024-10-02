aa7cc8f0f8f7018c2d7eff847ed7436e68c0cd67
restlessankyyy <rajankit749@gmail.com>
2024-10-02 23:58:49 +0000

    add github workflow

---
+++ main.tf
@@ -0,0 +1,49 @@
+# main.tf
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = file("path/to/your/credentials.json")  # replace this for local use
+}
+
+resource "google_cloud_run_service" "my_service" {
+  name     = "my-graphql-api"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.image_url
+        ports {
+          container_port = 8080
+        }
+        env {
+          name  = "COSMOS_DB_ENDPOINT"
+          value = var.cosmos_db_endpoint
+        }
+        env {
+          name  = "COSMOS_DB_KEY"
+          value = var.cosmos_db_key
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.my_service.location
+  service     = google_cloud_run_service.my_service.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
